1023282ddb7df769a518b2813780135e138e4d81
Nathan Bogan <bogannathan@gmail.com>
2017-10-10 20:20:34 -0400

    added authorization

--- src/components/search/searchresults.js
+++ src/components/search/searchresults.js
@@ -3,8 +3,18 @@ import axios from 'axios'
 import React, { Component } from 'react';
 import Search from './search';
 
-const spotifyUrl = 'https://api.spotify.com/v1/search?query=album:'
+const spotifyUrl = 'https://api.spotify.com/v1/search?q=album%3'
+const client_id = '4625fd59d3634a27ab6c9adedf1d163f'
+const client_secret = '5ff822075e1e447f9290edd378377d70'
+// const redirect = 'http://localhost:3000/callback'
 
+let config = {
+	headers: {
+		'Authorization': 'Basic ' + new Buffer(client_id + ':' + client_secret).toString('base64'),
+		'Accept':'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+	}
+}
 class SearchResults extends Component {
 	constructor(props) {
 		super(props);
@@ -16,7 +26,7 @@ class SearchResults extends Component {
 	}
 
 	albumSearch(term) {
-		axios.get(`${spotifyUrl}` + term)
+		axios.get(`${spotifyUrl}` + term + '&type=album', config)
 		.then(albums => {
 			console.log(albums)
 			console.log('find albums check')
